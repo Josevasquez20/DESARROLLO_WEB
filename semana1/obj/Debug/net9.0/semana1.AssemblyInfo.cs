b7175f0b153a218626cbfb2535d4efb4cbc3e4f7
Josevasquez20 <joseeliasvasquezguaygua11@gmail.com>
2025-12-21 18:07:54 -0500

    agregar
    agregar

--- semana1/obj/Debug/net9.0/semana1.AssemblyInfo.cs
+++ semana1/obj/Debug/net9.0/semana1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("semana1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b47b1e999b496b2cb1d69cfb7c8dc4ca905c19c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01153fe78f32cce6e8c3416790cfbd5a301936d")]
 [assembly: System.Reflection.AssemblyProductAttribute("semana1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("semana1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
