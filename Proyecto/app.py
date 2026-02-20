dc4065228319b28a3d8e2cb788a2fb018b90f144
Josevasquez20 <joseeliasvasquezguaygua11@gmail.com>
2026-02-20 10:01:49 -0500

    agregar
    agregar

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Ruta principal: renderiza index.html
 @app.route('/')
-def hello():
-    return "Hola, Flask!"
+def index():
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/producto/<nombre>')
-def producto(nombre):
-    return f'Producto: {nombre} – disponible.'
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
 
+@app.route('/factura')
+def factura():
+    return render_template('factura.html')
+
+@app.route('/contactos')
+def contactos():
+    return render_template('contactos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
