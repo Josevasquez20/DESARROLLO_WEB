ff2c5de80118209111247e7be4298619a1343e67
Josevasquez20 <joseeliasvasquezguaygua11@gmail.com>
2026-03-01 17:07:10 -0500

    .
    .

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,19 +1,73 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from form import ProductoForm
+from inventario.bd import init_db, get_db_connection
+from inventario.inventario import Inventario
+from inventario.producto import Producto
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='mi_clave_Secreta'
 
+init_db()
+inventario = Inventario()
+inventario.cargar_desde_db
 # Ruta principal: renderiza index.html
 @app.route('/')
-def index():
+def inicio():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#ruta de productos
+
+@app.route('/productos/nuevo', methods=['GET', 'POST'])
+def producto_nuevo():
+    form = ProductoForm()  # aquí sí defines el formulario
+
+    if form.validate_on_submit():  # método correcto
+        nombre = form.nombre.data
+        descripcion = form.descripcion.data
+        cantidad = form.cantidad.data
+        precio = form.precio.data
+        inventario.agregar_producto(nombre, descripcion, cantidad, precio)
+
+        flash('Porducto agregado extiosamente','success')
+
+        return redirect(url_for("productos_listar"))
+    return render_template("productos_form.html", form = form)
+
+#ruta para listar
 @app.route('/productos')
-def productos():
-    return render_template('productos.html')
+def productos_listar():
+    inventario.cargar_desde_db()
+    productos = list(inventario.productos.values())
+    return render_template('productos.html', productos=productos)
+
+
+
+
+ #ruta para editar
+# ruta para editar producto
+@app.route('/productos/editar/<int:id>', methods=['GET', 'POST'])
+def producto_editar(id):
+    productos = inventario.productos.get(id)
+    if not productos:
+     flash('Producto actualizado exitosamente', 'success')
+     return redirect(url_for('productos_listar'))
+
+    return render_template('productos.html',productos=productos)
+
+# ruta para eliminar producto
+@app.route('/productos/eliminar/<int:id>', methods=['POST'])
+def producto_eliminar(id):
+    inventario.eliminar_producto(id)
+    flash('Producto eliminado exitosamente', 'success')
+    return redirect(url_for('productos_listar'))
+
+
+
+
 
 @app.route('/factura')
 def factura():
---
+++ Proyecto/inventario/producto.py
@@ -0,0 +1,22 @@
+#clase producto 
+class Producto:
+    def __init__(self, id, nombre, descripcion, cantidad, precio):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def to_tuple(self):
+        return (self.id, self.self.nombre, self.self.descripcion, self.cantidad, self. self.precio)
+    
+    #diccionario 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'descripcion': self.descripcion,
+            'cantidad': self.cantidad,
+            'precio': self.precio
+        }
+
